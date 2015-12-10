2bd5d991e62699f6f878d28f8ee9998e7cafba73
Matthew Buck <freedomcobra@gmail.com>
2015-12-10 14:04:53 -0500

    added ruby logic exercises

---
+++ logical_operators.rb
@@ -0,0 +1,17 @@
+iAmChris = true
+iAmPurple = false
+iLikeFood = true
+iEatRocks = false
+
+puts (iAmChris and iLikeFood)
+puts (iLikeFood and iEatRocks)
+puts (iAmPurple and iLikeFood)
+puts (iAmPurple and iEatRocks)
+puts
+puts (iAmChris and iLikeFood)
+puts (iLikeFood and iEatRocks)
+puts (iAmPurple and iLikeFood)
+puts (iAmPurple and iEatRocks)
+puts
+puts (not iAmPurple)
+puts (not iAmChris)
